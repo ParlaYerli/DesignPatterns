d3bbafba5eaedaf73cfe73eb4f21dcc5800d250d
Ayşe Parla Yerli <parla.yerli@demirorenteknoloji.com>
2020-08-12 23:31:19 +0300

    threadsafesync add

--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,6 +1,9 @@
-﻿using Singleton.Example1;
+﻿using java.lang;
+using Singleton.Example1;
 using Singleton.Example2;
 using System;
+using System.Threading;
+using Thread = System.Threading.Thread;
 
 namespace Singleton
 {
@@ -20,15 +23,21 @@ namespace Singleton
             //////////////////Example2
             ///Eager Initialization
             var eagerInitialization = EagerInitialization.GetEagerInitialization();
-            Console.WriteLine("Eager initialization instance hashcode:{0}" , eagerInitialization.GetHashCode());
+            Console.WriteLine("Eager initialization instance hashcode:{0}", eagerInitialization.GetHashCode());
 
             ///Static Block Initialization
             var staticBlockInstance = StaticBlockInitialization.getInstance();
             Console.WriteLine("Static Block initialization instance hashcode:{0}", staticBlockInstance.GetHashCode());
             ///Lazy Initilization
             var lazyInitilization = LazyInitialization.getInstance();
-            Console.WriteLine("Lazy initialization instance hashcode:{0}",lazyInitilization.GetHashCode());
-            
+            Console.WriteLine("Lazy initialization instance hashcode:{0}", lazyInitilization.GetHashCode());
+            ///ThreadSafeSyncronized
+            ThreadSafeSyncronized threadSafe = ThreadSafeSyncronized.getInstance();
+            Thread thread1 = new Thread(new ThreadStart(threadSafe.sampleMethodA));
+            thread1.Start();
+            ThreadSafeSyncronized threadSafe2 = ThreadSafeSyncronized.getInstance();
+            Thread thread2 = new Thread(new ThreadStart(threadSafe2.sampleMethodB));
+            thread2.Start();
             Console.ReadKey();
         }
     }
